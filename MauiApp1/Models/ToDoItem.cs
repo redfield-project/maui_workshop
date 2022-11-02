094c07f4a13b748c864626eee116588f03e85f0a
Wojciech Kostrzewa <wkostrzewa@sii.pl>
2022-11-02 15:56:43 +0100

    Add service

---
+++ MauiApp1/Models/ToDoItem.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiApp1.Models;
+
+public class ToDoItem
+{
+    public class Class1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public string Image { get; set; }
+        public DateTime Deadline { get; set; }
+    }
+
+}
---
+++ MauiApp1/Services/ToDoItemsServicecs.cs
@@ -0,0 +1,28 @@
+﻿using MauiApp1.Models;
+using System.Net.Http.Json;
+
+namespace MauiApp1.Services;
+
+public class ToDoItemsServicecs
+{
+    HttpClient httpClient;
+    public ToDoItemsServicecs()
+    {
+        this.httpClient = new HttpClient();
+    }
+
+    List<ToDoItem> toDoItems;
+    public async Task<List<ToDoItem>> GetToDoItems()
+    {
+        if (toDoItems?.Count > 0)
+            return toDoItems;
+
+        var response = await httpClient.GetAsync("https://raw.githubusercontent.com/redfield-project/app_data/master/data.json");
+        if (response.IsSuccessStatusCode)
+        {
+            toDoItems = await response.Content.ReadFromJsonAsync<List<ToDoItem>>();
+        }
+
+        return toDoItems;
+    }
+}
